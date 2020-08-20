05798694472ac3d0468276c3f175758106082c4a
Alberto Correia <alberto_cpv@hotmail.com>
2020-08-21 01:09:19 +0200

    Provider and product routes

--- routes/provider.js
+++ routes/provider.js
@@ -3,68 +3,67 @@ const mongoose = require('mongoose')
 const router = express.Router()
 
 const Provider = require('../models/provider-model')
-const Product = require("../models/product-model")
 
-
+// Post route => to create one provider
 router.post('/providers', (req, res, next) => {
-  // req.body
   Provider.create({
-    name: req.body.name,  
-    adress:{
-      street : req.body.adress.street,
-      number : req.body.adress.number
-    }, 
-    telephone: req.body.telephone,
-    info: req.body.info
-  })
-  .then(response => {
-    console.log(response)
-    res.json(response)  
-  })
-  .catch(err => {
-    console.log(err)
-    res.json(err)
-  })
+      name: req.body.name,
+      adress: {
+        street: req.body.adress.street,
+        number: req.body.adress.number
+      },
+      telephone: req.body.telephone,
+      info: req.body.info
+    })
+    .then(response => {
+      console.log(response)
+      res.json(response)
+    })
+    .catch(erroror => {
+      console.log(error)
+      res.json(error)
+    })
 })
+// GET route => to find and return all provider list
+router.get('/providers', (req, res, next) => {
+  Provider.find()
+    .then(providers => {
+      console.log(providers)
+      res.json(providers)
+    })
+    .catch(error => {
+      res.json(error)
+    })
 
-/*router.get('/provider', (req, res, next) => {
-  // recoger TODOS los proyectos, y devolver como JSON
-  // TO-DO: popular con las productos
-  provider.find()
-  .populate('products')
-  .then(provider => {
-    res.json(provider)
-  })
-  .catch(err => {
-    res.json(err)
-  })
-
-})*/
-
+})
+// GET route => to find and return an especific provider
 router.get('/providers/:id', (req, res, next) => {
   console.log(req.params.id)
   Provider.findById(req.params.id)
- // .populate('products')
-  .then(provider => {
-    res.json(provider)
-  })
-  .catch(err => {
-    console.log(err)
-    res.json(err)
-  })
-  
+    .then(provider => {
+      console.log(provider)
+      res.json(provider)
+    })
+    .catch(error => {
+      console.log(error)
+      res.json(error)
+    })
+
 })
 
 // PUT route => to update a specific provider
-router.put('/provider/:id', (req, res, next) => {
+router.put('/providers/:id', (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    res.status(400).json({ message: 'Specified id is not valid' });
+    res.status(400).json({
+      message: 'Specified id is not valid'
+    });
     return;
   }
-
   Provider.findByIdAndUpdate(req.params.id, req.body)
     .then(() => {
-      res.json({ message: `provider with ${req.params.id} is updated successfully.` });
+      res.json({
+        message: `provider with ${req.params.id} is updated successfully.`
+      });
     })
     .catch(error => {
       res.json(error);
@@ -72,15 +71,19 @@ router.put('/provider/:id', (req, res, next) => {
 });
 
 // DELETE route => to delete a specific provider
-router.delete('/provider/:id', (req, res, next) => {
+router.delete('/providers/:id', (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    res.status(400).json({ message: 'Specified id is not valid' });
+    res.status(400).json({
+      message: 'Specified id is not valid'
+    });
     return;
   }
 
   Provider.findByIdAndRemove(req.params.id)
     .then(() => {
-      res.json({ message: `Provider with ${req.params.id} is removed successfully.` });
+      res.json({
+        message: `Provider with ${req.params.id} is removed successfully.`
+      });
     })
     .catch(error => {
       res.json(error);
