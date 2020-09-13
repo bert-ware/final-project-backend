52977738ecc72e07fda557cf814a211b72d98fc2
Alberto Correia <alberto_cpv@hotmail.com>
2020-09-13 20:13:07 +0200

    fileupoad recipes endpoint

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const router = express.Router()
-
+const uploader = require('../configs/cloudinary')
 const Recipe = require("../models/recipe-model")
 const Product = require("../models/product-model")
 
@@ -82,6 +82,25 @@ router.delete('/recipes/:id', (req, res, next) => {
         res.json({
           message: `Recipe with ${req.params.id} removed successfully.`
         })
+      })
+      .catch(error => {
+        res.json(error)
+      })
+  })
+   // PUT route => to be used as recipe fileuload endpoint
+   router.put('/recipes/image/:id', uploader.single("myFile"), (req, res, next) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      res.status(400).json({
+        message: 'Specified id is not valid'
+      })
+      return
+    }
+    console.log(req.file)
+    Recipe.findByIdAndUpdate(req.params.id, {recipeImgUrl : req.file.path})
+      .then((recipe) => {
+        res.json(
+            recipe
+        )
       })
       .catch(error => {
         res.json(error)
