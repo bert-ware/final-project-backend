6aa17b5c035ad8bf781383b532294d71e80b912d
Alberto Correia <alberto_cpv@hotmail.com>
2020-10-13 22:08:34 +0200

    deploy V1.3

---
+++ public/precache-manifest.a4b93c2b4abeca8cf022c98896eb32cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ca6442e2b494ceb8ca30f45280a948",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6097ca953c28c125d870",
+    "url": "/static/css/2.9499c6f2.chunk.css"
+  },
+  {
+    "revision": "26ee6477723de8efbd7c",
+    "url": "/static/css/main.c7c6b8b3.chunk.css"
+  },
+  {
+    "revision": "6097ca953c28c125d870",
+    "url": "/static/js/2.5259a782.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5259a782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ee6477723de8efbd7c",
+    "url": "/static/js/main.430a3421.chunk.js"
+  },
+  {
+    "revision": "452bad7e14cccc6d3a58",
+    "url": "/static/js/runtime-main.a7e3f6a9.js"
+  }
+]);
